ffed2f13fbc1b24ae2b40f5096f80c1da7463ed1
chenfeiyu <chenfeiyu@youzan.com>
2024-12-05 11:01:26 +0800

    feat: 整合装备系统

--- src/data/items/index.ts
+++ src/data/items/index.ts
@@ -1,8 +1,10 @@
 import { Item, ItemId } from '../../constants/item';
 import { CONSUMABLES } from './consumables';
 import { GEARS } from './gears';
+import { ARMORS } from './gears/armors';
 
 export const ITEMS: Record<ItemId, Item> = {
     ...CONSUMABLES,
-    ...GEARS
+    ...GEARS,
+    ...ARMORS
 }
